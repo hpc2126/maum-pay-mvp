1134d0657f2a29f16a79e333c9e38afe765a6871
BigBoss <you@example.com>
2026-01-31 23:43:15 +0900

    fix: sender page imports SenderClient

--- app/guest/sender/page.tsx
+++ app/guest/sender/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import SenderClient from "./SenderClient";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default function Page() {
   return (
